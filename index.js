9cb5960c4a297fe39c4fa1f5ca1f8a31151cd250
harmit <hphamit@gmail.com>
2024-01-14 01:38:15 +0530

    Final

---
+++ index.js
@@ -0,0 +1,29 @@
+import qr  from "qr-image";
+import inquirer from 'inquirer';
+import fs from 'fs';
+
+inquirer
+  .prompt([
+    {
+        "message" : "Enter URL to generate QR Code :",
+        "name" : "URL"
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_image.png'));
+
+    fs.writeFile('URL.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }); 
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
